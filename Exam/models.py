a84c86eedd9024a55c3ef32855d5a4cdd951cbac
Khamidkhanov Muhammadzohid <valihon555@gmail.com>
2025-09-22 16:36:43 +0500

    models.py

--- Exam/models.py
+++ Exam/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class ListeningTest(models.Model):
+    questions = models.JSONField()   # list ko‘rinishida saqlash
+    audio = models.FileField(upload_to="listening/")
+    answers = models.JSONField()
+
+    def __str__(self):
+        return f"Listening Test {self.id}"
+
+
+class ReadingTest(models.Model):
+    text_field = models.TextField()
+    questions = models.JSONField()
+    # answers = models.JSONField()
+
+    def __str__(self):
+        return f"Reading Test {self.id}"
+
+
+class WritingTest(models.Model):
+    question = models.TextField()
+    answer_text = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Writing Test {self.id}"
+
+
+class Exam(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_public = models.BooleanField(default=False)
+
+    listening = models.ForeignKey(ListeningTest, on_delete=models.SET_NULL, null=True, blank=True)
+    reading = models.ForeignKey(ReadingTest, on_delete=models.SET_NULL, null=True, blank=True)
+    writing = models.ForeignKey(WritingTest, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"Exam {self.id} - {self.user.username}"
