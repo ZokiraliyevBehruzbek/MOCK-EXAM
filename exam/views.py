d5c6b379a898df20405d01e7b35c223064c19b03
khasanbayev-7 <khasanbayev.7@mail.ru>
2025-09-25 22:06:12 +0500

    Exam API qo‘shildi: join_exam va permissionlar

--- exam/views.py
+++ exam/views.py
@@ -1,37 +1,50 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import ListeningTest, ReadingTest, WritingTest, Exam
 from .serializers import (
     ListeningTestSerializer, ReadingTestSerializer,
     WritingTestSerializer, ExamSerializer
 )
 from .permissions import IsAdminOrReadOnly, IsUserInExam
+from rest_framework.permissions import IsAuthenticated
 
 
 class ListeningTestViewSet(viewsets.ModelViewSet):
     queryset = ListeningTest.objects.all()
     serializer_class = ListeningTestSerializer
-    permission_classes = [IsAdminOrReadOnly, IsUserInExam]
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class ReadingTestViewSet(viewsets.ModelViewSet):
     queryset = ReadingTest.objects.all()
     serializer_class = ReadingTestSerializer
-    permission_classes = [IsAdminOrReadOnly, IsUserInExam]
-
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class WritingTestViewSet(viewsets.ModelViewSet):
     queryset = WritingTest.objects.all()
     serializer_class = WritingTestSerializer
-    permission_classes = [IsAdminOrReadOnly, IsUserInExam]
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class ExamViewSet(viewsets.ModelViewSet):
     queryset = Exam.objects.filter(is_public=True)
     serializer_class = ExamSerializer
-    permission_classes = [IsAdminOrReadOnly, IsUserInExam]
 
+    permission_classes = [IsAdminOrReadOnly]
 
     def perform_create(self, serializer):
         exam = serializer.save()
         exam.users.add(self.request.user)
+
+    @action(detail=True, methods=["post"], url_path="join", permission_classes=[IsAuthenticated])
+    def join_exam(self, request, pk=None):
+        exam = self.get_object()
+        user = request.user
+
+        if user in exam.users.all():
+            return Response({"detail": "You already joined this exam."}, status=status.HTTP_400_BAD_REQUEST)
+
+        exam.users.add(user)
+        return Response({"detail": "Successfully joined the exam."}, status=status.HTTP_200_OK)
