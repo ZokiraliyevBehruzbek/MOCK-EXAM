b9d8e42996af776c1ab6837a34106a761d526522
ZokiraliyevBehruzbek <behruzbekzokiraliyev@gmail.com>
2025-09-26 22:56:23 +0500

    update views add permissions

--- exam/views.py
+++ exam/views.py
@@ -32,6 +32,152 @@ class WritingTestViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAdminUser]
 
 
+# class ExamViewSet(viewsets.ModelViewSet):
+#     queryset = Exam.objects.all()
+#     serializer_class = ExamSerializer
+#     permission_classes = [IsAdminOrReadOnly]
+
+#     @action(detail=True, methods=["post"], url_path="join", permission_classes=[IsAuthenticated])
+#     def join_exam(self, request, pk=None):
+#         exam = self.get_object()
+#         user = request.user
+
+#         if exam.start_time < timezone.now():
+#             return Response({"detail": "exam already started"}, status=status.HTTP_400_BAD_REQUEST)
+
+#         if not user in exam.allowed_users.all() and not exam.is_public:
+#             return Response({"detail": "You are not allowed"}, status=status.HTTP_400_BAD_REQUEST)
+
+#         exam.joined_users.add(user)
+#         return Response({"detail": "Successfully joined the exam."}, status=status.HTTP_200_OK)
+    
+#     @action(detail=True, methods=['post'], url_path='leave', permission_classes=[IsAuthenticated])
+#     def leave_exam(self, request, pk=None):
+#         exam = self.get_object()
+#         exam.joined_users.get(id=request.user.id).delete()
+#         return Response({"detail": "you left the exam"})
+    
+#     @action(detail=True, methods=['post'], url_path='start-listening', permission_classes=[IsAuthenticated])
+#     def start_listening(self, request, pk=None):
+#         exam = self.get_object()
+#         user = request.user
+
+#         if exam.start_time > timezone.now() or exam.end_time < timezone.now():
+#             print(exam.start_time, exam.end_time, timezone.now(), exam.start_time > timezone.now(), exam.end_time < timezone.now())
+#             raise PermissionDenied('not the right time')
+#         if not user in exam.joined_users.all():
+#             return Response({"detail": "You have not joined"}, status=status.HTTP_400_BAD_REQUEST)
+
+#         try:
+#             UserExamSession.objects.get(exam=exam, user=user)
+#             raise PermissionDenied("you already started listening")
+#         except UserExamSession.DoesNotExist:
+#             pass
+
+#         UserExamSession.objects.create(exam=exam, user=user, listening_started_at=timezone.now())
+
+#         serializer = ListeningTestSerializer(exam.listening)
+#         return Response(serializer.data)
+    
+#     @action(detail=True, methods=['post'], url_path='finish-listening', permission_classes=[IsAuthenticated])
+#     def finish_listening(self, request, pk=None):
+#         exam = self.get_object()
+#         user = request.user
+#         answers = request.data
+
+#         try:
+#             session = UserExamSession.objects.get(exam=exam, user=user)
+#             if timezone.now() - session.listening_started_at > timedelta(minutes=32):
+#                 raise PermissionDenied("Too late. Womp Womp")
+#             TestResult.objects.create(user=user, 
+#                                       exam=exam, 
+#                                       test_type='listening', 
+#                                       answers=answers) 
+#             session.listening_finished = True
+#             session.save()
+#             return Response({"detail": "Congratulations! Go on now to reading!"})
+#         except:
+#             raise PermissionDenied("did you even start listening?")
+    
+#     @action(detail=True, methods=['post'], url_path='start-reading', permission_classes=[IsAuthenticated])
+#     def start_reading(self, request, pk=None):
+#         exam = self.get_object()
+#         user = request.user
+
+#         try:
+#             session = UserExamSession.objects.get(exam=exam, user=user)
+#             if session.reading_finished:
+#                 raise PermissionDenied("you have already finished reading")
+#             if not session.listening_finished:
+#                 # listening tugatib, reading boshlash kerak
+#                 raise PermissionDenied("you haven't finsihed listening")
+#             session.reading_started_at = timezone.now()
+#             session.save()
+#         except UserExamSession.DoesNotExist:
+#             raise PermissionDenied("reading not started")
+
+#         serializer = ReadingTestSerializer(exam.reading)
+#         return Response(serializer.data)
+    
+#     @action(detail=True, methods=['post'], url_path='finish-reading', permission_classes=[IsAuthenticated])
+#     def finish_reading(self, request, pk=None):
+#         exam = self.get_object()
+#         user = request.user
+#         answers = request.data
+
+#         try:
+#             session = UserExamSession.objects.get(exam=exam, user=user)
+#             if session.reading_finished:
+#                 raise PermissionDenied("reading already finished")
+#             if timezone.now() - session.reading_started_at > timedelta(minutes=60):
+#                 raise PermissionDenied("Too late. Womp Womp")
+#             TestResult.objects.create(user=user, 
+#                                       exam=exam, 
+#                                       test_type='reading', 
+#                                       answers=answers)
+#             session.reading_finished = True
+#             session.save()
+#             return Response({"detail": "Congratulations! Go on now to writing!"})
+#         except:
+#             raise PermissionDenied("did you even start listening?")
+    
+#     @action(detail=True, methods=['post'], url_path='start-writing', permission_classes=[IsAuthenticated])
+#     def start_writing(self, request, pk=None):
+#         exam = self.get_object()
+#         user = request.user
+
+#         try:
+#             session = UserExamSession.objects.get(exam=exam, user=user)
+#             if not session.reading_finished:
+#                 raise PermissionDenied("you haven't finished reading")
+#             session.writing_started_at = timezone.now()
+#             session.save()
+#         except UserExamSession.DoesNotExist:
+#             raise PermissionDenied("exam session has not been started, first start listening to create a start session")
+        
+#         serializer = WritingTestSerializer(exam.writing)
+#         return Response(serializer.data)
+
+#     @action(detail=True, methods=['post'], url_path='finish-writing', permission_classes=[IsAuthenticated])
+#     def finish_writing(self, request, pk=None):
+#         exam = self.get_object()
+#         user = request.user
+#         answers = request.data
+
+#         try:
+#             session = UserExamSession.objects.get(exam=exam, user=user)
+#             if timezone.now() - session.writing_started_at > timedelta(minutes=60):
+#                 raise PermissionDenied("Too late. Womp Womp")
+#             TestResult.objects.create(user=user, 
+#                                       exam=exam, 
+#                                       test_type='writing', 
+#                                       answers=answers)
+#             session.delete()
+#             exam.allowed_users.get(id=request.user.id).delete()
+#             return Response({"detail": "That's it! Just wait for the results!"})
+#         except:
+#             raise PermissionDenied("did you even start listening?")
+
 class ExamViewSet(viewsets.ModelViewSet):
     queryset = Exam.objects.all()
     serializer_class = ExamSerializer
@@ -45,41 +191,34 @@ class ExamViewSet(viewsets.ModelViewSet):
         if exam.start_time < timezone.now():
             return Response({"detail": "exam already started"}, status=status.HTTP_400_BAD_REQUEST)
 
-        if not user in exam.allowed_users.all() and not exam.is_public:
+        if not exam.is_public and user not in exam.allowed_users.all():
             return Response({"detail": "You are not allowed"}, status=status.HTTP_400_BAD_REQUEST)
 
         exam.joined_users.add(user)
         return Response({"detail": "Successfully joined the exam."}, status=status.HTTP_200_OK)
-    
-    @action(detail=True, methods=['post'], url_path='leave', permission_classes=[IsAuthenticated])
+
+    @action(detail=True, methods=['post'], url_path='leave', permission_classes=[IsAuthenticated, IsUserInExam])
     def leave_exam(self, request, pk=None):
         exam = self.get_object()
-        exam.joined_users.get(id=request.user.id).delete()
-        return Response({"detail": "you left the exam"})
-    
-    @action(detail=True, methods=['post'], url_path='start-listening', permission_classes=[IsAuthenticated])
+        exam.joined_users.remove(request.user)
+        return Response({"detail": "You left the exam."})
+
+    @action(detail=True, methods=['post'], url_path='start-listening', permission_classes=[IsAuthenticated, IsUserInExam])
     def start_listening(self, request, pk=None):
         exam = self.get_object()
         user = request.user
 
         if exam.start_time > timezone.now() or exam.end_time < timezone.now():
-            print(exam.start_time, exam.end_time, timezone.now(), exam.start_time > timezone.now(), exam.end_time < timezone.now())
             raise PermissionDenied('not the right time')
-        if not user in exam.joined_users.all():
-            return Response({"detail": "You have not joined"}, status=status.HTTP_400_BAD_REQUEST)
 
-        try:
-            UserExamSession.objects.get(exam=exam, user=user)
+        if UserExamSession.objects.filter(exam=exam, user=user).exists():
             raise PermissionDenied("you already started listening")
-        except UserExamSession.DoesNotExist:
-            pass
 
         UserExamSession.objects.create(exam=exam, user=user, listening_started_at=timezone.now())
-
         serializer = ListeningTestSerializer(exam.listening)
         return Response(serializer.data)
-    
-    @action(detail=True, methods=['post'], url_path='finish-listening', permission_classes=[IsAuthenticated])
+
+    @action(detail=True, methods=['post'], url_path='finish-listening', permission_classes=[IsAuthenticated, IsUserInExam])
     def finish_listening(self, request, pk=None):
         exam = self.get_object()
         user = request.user
@@ -89,17 +228,15 @@ class ExamViewSet(viewsets.ModelViewSet):
             session = UserExamSession.objects.get(exam=exam, user=user)
             if timezone.now() - session.listening_started_at > timedelta(minutes=32):
                 raise PermissionDenied("Too late. Womp Womp")
-            TestResult.objects.create(user=user, 
-                                      exam=exam, 
-                                      test_type='listening', 
-                                      answers=answers) 
+
+            TestResult.objects.create(user=user, exam=exam, test_type='listening', answers=answers)
             session.listening_finished = True
             session.save()
             return Response({"detail": "Congratulations! Go on now to reading!"})
-        except:
+        except UserExamSession.DoesNotExist:
             raise PermissionDenied("did you even start listening?")
-    
-    @action(detail=True, methods=['post'], url_path='start-reading', permission_classes=[IsAuthenticated])
+
+    @action(detail=True, methods=['post'], url_path='start-reading', permission_classes=[IsAuthenticated, IsUserInExam])
     def start_reading(self, request, pk=None):
         exam = self.get_object()
         user = request.user
@@ -109,8 +246,8 @@ class ExamViewSet(viewsets.ModelViewSet):
             if session.reading_finished:
                 raise PermissionDenied("you have already finished reading")
             if not session.listening_finished:
-                # listening tugatib, reading boshlash kerak
-                raise PermissionDenied("you haven't finsihed listening")
+                raise PermissionDenied("you haven't finished listening")
+
             session.reading_started_at = timezone.now()
             session.save()
         except UserExamSession.DoesNotExist:
@@ -118,8 +255,8 @@ class ExamViewSet(viewsets.ModelViewSet):
 
         serializer = ReadingTestSerializer(exam.reading)
         return Response(serializer.data)
-    
-    @action(detail=True, methods=['post'], url_path='finish-reading', permission_classes=[IsAuthenticated])
+
+    @action(detail=True, methods=['post'], url_path='finish-reading', permission_classes=[IsAuthenticated, IsUserInExam])
     def finish_reading(self, request, pk=None):
         exam = self.get_object()
         user = request.user
@@ -131,17 +268,15 @@ class ExamViewSet(viewsets.ModelViewSet):
                 raise PermissionDenied("reading already finished")
             if timezone.now() - session.reading_started_at > timedelta(minutes=60):
                 raise PermissionDenied("Too late. Womp Womp")
-            TestResult.objects.create(user=user, 
-                                      exam=exam, 
-                                      test_type='reading', 
-                                      answers=answers)
+
+            TestResult.objects.create(user=user, exam=exam, test_type='reading', answers=answers)
             session.reading_finished = True
             session.save()
             return Response({"detail": "Congratulations! Go on now to writing!"})
-        except:
-            raise PermissionDenied("did you even start listening?")
-    
-    @action(detail=True, methods=['post'], url_path='start-writing', permission_classes=[IsAuthenticated])
+        except UserExamSession.DoesNotExist:
+            raise PermissionDenied("did you even start reading?")
+
+    @action(detail=True, methods=['post'], url_path='start-writing', permission_classes=[IsAuthenticated, IsUserInExam])
     def start_writing(self, request, pk=None):
         exam = self.get_object()
         user = request.user
@@ -150,15 +285,16 @@ class ExamViewSet(viewsets.ModelViewSet):
             session = UserExamSession.objects.get(exam=exam, user=user)
             if not session.reading_finished:
                 raise PermissionDenied("you haven't finished reading")
+
             session.writing_started_at = timezone.now()
             session.save()
         except UserExamSession.DoesNotExist:
-            raise PermissionDenied("exam session has not been started, first start listening to create a start session")
-        
+            raise PermissionDenied("exam session has not been started, first start listening")
+
         serializer = WritingTestSerializer(exam.writing)
         return Response(serializer.data)
 
-    @action(detail=True, methods=['post'], url_path='finish-writing', permission_classes=[IsAuthenticated])
+    @action(detail=True, methods=['post'], url_path='finish-writing', permission_classes=[IsAuthenticated, IsUserInExam])
     def finish_writing(self, request, pk=None):
         exam = self.get_object()
         user = request.user
@@ -168,12 +304,10 @@ class ExamViewSet(viewsets.ModelViewSet):
             session = UserExamSession.objects.get(exam=exam, user=user)
             if timezone.now() - session.writing_started_at > timedelta(minutes=60):
                 raise PermissionDenied("Too late. Womp Womp")
-            TestResult.objects.create(user=user, 
-                                      exam=exam, 
-                                      test_type='writing', 
-                                      answers=answers)
+
+            TestResult.objects.create(user=user, exam=exam, test_type='writing', answers=answers)
             session.delete()
-            exam.allowed_users.get(id=request.user.id).delete()
+            exam.joined_users.remove(user)
             return Response({"detail": "That's it! Just wait for the results!"})
-        except:
-            raise PermissionDenied("did you even start listening?")
+        except UserExamSession.DoesNotExist:
+            raise PermissionDenied("did you even start writing?")
